18506e9d7a237f5ad1bad3c9e6fe3f5be9469291
alanmclachlan <alan.mcl@gmail.com>
2018-03-08 14:33:13 +0200

    paladin ability: lay on hands

---
+++ data/default/src/mclachlan/maze/campaign/def/stat/magic/LayOnHandsSpellResult.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2011 Alan McLachlan
+ *
+ * This file is part of Escape From The Maze.
+ *
+ * Escape From The Maze is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mclachlan.maze.campaign.def.stat.magic;
+
+import java.util.*;
+import mclachlan.maze.game.MazeEvent;
+import mclachlan.maze.stat.Dice;
+import mclachlan.maze.stat.UnifiedActor;
+import mclachlan.maze.stat.combat.event.HealingEvent;
+import mclachlan.maze.stat.magic.Spell;
+import mclachlan.maze.stat.magic.SpellEffect;
+import mclachlan.maze.stat.magic.SpellResult;
+
+/**
+ * A spell result that opens lock or disarms traps
+ */
+public class LayOnHandsSpellResult extends SpellResult
+{
+	@Override
+	public List<MazeEvent> apply(UnifiedActor source, UnifiedActor target,
+		int castingLevel, SpellEffect parent, Spell spell)
+	{
+		int target_healing = new Dice(castingLevel, 6, 0).roll();
+
+		List<MazeEvent> result = new ArrayList<MazeEvent>();
+
+		result.add(new HealingEvent(target, target_healing));
+		result.add(new HealingEvent(source, castingLevel));
+
+		return result;
+	}
+}
